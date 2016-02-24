62bcf2438a5faf2c8344930b77c5b6317fda874c
Michael DuBois <michael@michaeldubois.me>
2016-02-23 23:44:30 -0800

    proposed fix for factory inheritance.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+const _global = typeof window !== 'undefined' ? window : global;
 function toFactory(Class) {
   const Factory = function(...args) {
-    return new Class(...args)
+    let result;
+    if (typeof this !== 'undefined' && this !== (_global)) {
+      result = Class.call(this, ...args);
+    } else {
+      result = new Class(...args)
+    }
+    return result;
   }
   Factory.__proto__ = Class
   Factory.prototype = Class.prototype
--- to-factory.js
+++ to-factory.js
@@ -1,12 +1,20 @@
-"use strict";
+'use strict';
 
+var _global = typeof window !== 'undefined' ? window : global;
 function toFactory(Class) {
   var Factory = function Factory() {
+    var result = undefined;
+
     for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
       args[_key] = arguments[_key];
     }
 
-    return new (Function.prototype.bind.apply(Class, [null].concat(args)))();
+    if (typeof this !== 'undefined' && this !== _global) {
+      result = Class.call.apply(Class, [this].concat(args));
+    } else {
+      result = new (Function.prototype.bind.apply(Class, [null].concat(args)))();
+    }
+    return result;
   };
   Factory.__proto__ = Class;
   Factory.prototype = Class.prototype;
